a84d784b81f36f8c96e500a4f2c1197b2e484514
Tullum <42997866+Tullum@users.noreply.github.com>
2019-12-12 11:33:52 +0100

    Add files via upload

---
+++ netlify-vue-test/dist/precache-manifest.aa84f3a3876e1159d41a7416a8286577.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7d3f0efa2e28a78c2d",
+    "url": "/css/app.b68c65d9.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d95bcfb75dc5835d00573fe735b6df6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "beb330ea9b5ef49ca336",
+    "url": "/js/about.7934af2e.js"
+  },
+  {
+    "revision": "ff7d3f0efa2e28a78c2d",
+    "url": "/js/app.14b475dc.js"
+  },
+  {
+    "revision": "b1c09b85a35f3fc4a0e9",
+    "url": "/js/chunk-vendors.5483cdca.js"
+  },
+  {
+    "revision": "314cfaab877943827a3823b890c1dcda",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
