3881b19686c3d6a73c1792d1ffad8fc76b42c642
Tine Deleuran Ullum <tine3488@edu.eal.dk>
2019-12-15 20:55:10 +0100

    table undone

--- netlify-vue-test/src/views/About.vue
+++ netlify-vue-test/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
+  <div class="table">
     <h1>This is an about page</h1>
+    <TableStructure></TableStructure>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import TableStructure from "@/components/Table.vue";
+
+export default {
+  name: "table",
+  components: {
+    TableStructure
+  }
+};
+</script>
